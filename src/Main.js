90fb288522dd566de01dfaede4cb2657c0b42d81
Daniil Dubrava <daniil.dubrava@ddgcorp.ru>
2019-03-09 18:12:13 +0300

    feat: add icon with menu in tray

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu, Tray } = require('electron');
 const path = require('path');
 
 function createWindow () {
@@ -17,7 +17,22 @@ function createWindow () {
   win.loadFile(path.resolve(__dirname, 'index/index.html'));
 };
 
-app.on('ready', createWindow);
+const addInTray = () => {
+  const tray = new Tray(path.join(__dirname, 'assets/tray.ico'));
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Search for Login', type: 'normal' },
+    { label: 'Open', type: 'normal' },
+    { label: 'Settings', type: 'normal' },
+    { label: 'Close', type: 'normal' }
+  ]);
+  tray.setToolTip('EncryptStore');
+  tray.setContextMenu(contextMenu);
+};
+
+app.on('ready', () => {
+  createWindow();
+  addInTray();
+});
 
 app.on('window-all-closed', function () {
   app.quit();
