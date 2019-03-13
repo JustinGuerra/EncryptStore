6dd3044153c8c84782ac7ddf38f2fc0d64c4ca89
Daniel Hix <danieljacobhix@gmail.com>
2019-03-13 17:11:06 -0500

    added defaults to the data file class so it will auto initialize

--- app/src/models/data-file.ts
+++ app/src/models/data-file.ts
@@ -1,8 +1,9 @@
 import { entry } from "./entry";
+import * as fs from 'fs';
+import * as path from 'path';
 
+//TODO this needs to require a password hash to build.
 export default class DataFile {
-    public hash: string;
-    public contents: entry[];
-
-    constructor() {}
+    public hash: string = '';
+    public contents: entry[] = [];
 }
