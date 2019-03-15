b7b6cd1b1c1316a39aa6bffebe1e3899c4722d1a
Daniel Hix <danieljacobhix@gmail.com>
2019-03-14 20:53:33 -0500

    Added prettier and fixed eslint warnings

--- app/src/data-file-manager.ts
+++ app/src/data-file-manager.ts
@@ -12,35 +12,35 @@ let dataFileName: string = 'data.esdf';
 //This will be used to manage the data file
 //to initialize you need to pass in a path to the data file,
 //if it recieves none then it will generate a new one.
-export default class DataFileManager{
+export default class DataFileManager {
 
-    public dataFile: DataFile;
+  public dataFile: DataFile;
 
-    constructor(hash: string, dataFilePath: string){
+  constructor(hash: string, dataFilePath: string) {
 
-            this.getPathStats(dataFilePath)
-                .then( statsSuccess => {
-                    //There was no error codes and so we got a stat object
-                    console.log(statsSuccess);
-                })
-                .catch( statsError =>{
-                    //There was an error code
-                    console.log(statsError);
+    this.getPathStats(dataFilePath)
+      .then(statsSuccess => {
+        //There was no error codes and so we got a stat object
+        console.log(statsSuccess);
+      })
+      .catch(statsError => {
+        //There was an error code
+        console.log(statsError);
 
-                });
-    }
+      });
+  }
 
-    //To generate a file we need a file path
-    //if we don't have one then we'll just make one at our current path
-    //I don't think this will work though and we should use appData possibly.
-    async generateNewFile(hash: string, directoryPath = __dirname) {
-        let filePath: string = path.join(directoryPath, 'data.esdf');
-        let dataFile: DataFile = new DataFile(hash);
+  //To generate a file we need a file path
+  //if we don't have one then we'll just make one at our current path
+  //I don't think this will work though and we should use appData possibly.
+  async generateNewFile(hash: string, directoryPath = __dirname) {
+    let filePath: string = path.join(directoryPath, 'data.esdf');
+    let dataFile: DataFile = new DataFile(hash);
 
-        await writeFile( filePath, JSON.stringify(dataFile));
-    }
+    await writeFile(filePath, JSON.stringify(dataFile));
+  }
 
-    async getPathStats(filePath: string): Promise<fs.Stats> {
-        return await fsStat(filePath);
-    }
+  async getPathStats(filePath: string): Promise<fs.Stats> {
+    return await fsStat(filePath);
+  }
 }
